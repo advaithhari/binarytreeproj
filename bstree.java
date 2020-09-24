6e9a3d332007746b57146f7019ab31f5fbd34485
Advaith Hari <33227909+advaithhari@users.noreply.github.com>
2020-09-24 15:08:38 -0400

    Add files via upload

---
+++ bstree.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class bstree {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
